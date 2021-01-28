2296c84c010ee80b991d2187700b5db49cc5f551
Davi Lucas <davilucasdlcreator@gmail.com>
2021-01-28 14:07:32 -0300

    :sparkles: feat: screenshot in index.js

---
+++ prepost/src/index.js
@@ -0,0 +1,13 @@
+const puppeteer = require('puppeteer')
+
+async function screenshot(url){
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+
+    await page.goto(url);
+    await page.screenshot({ path:"screenshot.png" });
+
+    await browser.close();
+}
+
+screenshot("https://google.com/")
